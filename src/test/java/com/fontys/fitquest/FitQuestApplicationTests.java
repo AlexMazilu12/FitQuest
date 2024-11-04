4e5c2000ec0bdcb119e6e76c79dddd0518ff2fe4
Alexandru Mazilu <I525933@fhict.nl>
2024-11-04 14:17:58 +0100

    test fail resolved

--- src/test/java/com/fontys/fitquest/FitQuestApplicationTests.java
+++ src/test/java/com/fontys/fitquest/FitQuestApplicationTests.java
@@ -2,12 +2,13 @@ package com.fontys.fitquest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FitQuestApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }

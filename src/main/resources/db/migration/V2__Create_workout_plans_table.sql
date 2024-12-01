cb0ad24ae6672d86df51bc47acfec94b36c80d7c
Alexandru Mazilu <I525933@fhict.nl>
2024-12-01 19:35:52 +0100

    flyway migration update

---
+++ src/main/resources/db/migration/V2__Create_workout_plans_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS workout_plans (
+                                             id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                             user_id INT NOT NULL,
+                                             title VARCHAR(255) NOT NULL,
+                                             description TEXT,
+                                             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                                             updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+                                             FOREIGN KEY (user_id) REFERENCES users(id)
+);

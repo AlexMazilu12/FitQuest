7abcd4f49607e04cc606d3f0be2e293c29140bc5
Alexandru Mazilu <I525933@fhict.nl>
2025-01-15 15:09:20 +0100

    deployment finnaly works, integrated it in pipeline also

--- src/main/java/com/fontys/fitquest/persistence/WorkoutPlanRepository.java
+++ src/main/java/com/fontys/fitquest/persistence/WorkoutPlanRepository.java
@@ -2,10 +2,15 @@ package com.fontys.fitquest.persistence;
 
 import com.fontys.fitquest.persistence.entity.WorkoutPlanEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface WorkoutPlanRepository extends JpaRepository<WorkoutPlanEntity, Long> {
     boolean existsByUserIdAndTitle(int userId, String title);
     Optional<WorkoutPlanEntity> findById(Long id);
+
+    @Query("SELECT u.id, u.email, COUNT(wp) FROM WorkoutPlanEntity wp JOIN UserEntity u ON wp.userId = u.id GROUP BY u.id, u.email")
+    List<Object[]> countWorkoutPlansByUserWithDetails();
 }
